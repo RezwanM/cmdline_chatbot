e7d0636e1ac462477ba4f1452fe859f2f8d4177e
Rezwan Matin <rezwanmatin@gmail.com>
2025-07-31 16:01:18 -0400

    Added option to clear window; added type hints; reformatted main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from dotenv import load_dotenv 
+import os
+from dotenv import load_dotenv
 from google import genai
 
 
@@ -6,18 +7,19 @@ load_dotenv()
 
 client = genai.Client()
 
-def chat_with_gemini(prompt):
-    response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=prompt
-    )
 
+def chat_with_gemini(prompt: str) -> str:
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
     return response.text
+
 
 if __name__ == "__main__":
     while True:
         user_input = input("You: ")
         if user_input.lower() in ["quit", "exit", "bye"]:
             break
-        
-        response = chat_with_gemini(user_input)
-        print("Chatbot: ", response)
+        elif user_input.lower() == "clear":
+            os.system("cls" if os.name == "nt" else "clear")
+        else:
+            response = chat_with_gemini(user_input)
+            print("Chatbot: ", response)
